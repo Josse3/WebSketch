b1378caa7a3b882b95f66695452526bb7a8bccbf
Josse3 <josse.cottenier@hotmail.com>
2019-07-21 15:29:05 +0200

    Added component replacing div as drawing box

--- ui-vue/src/views/Drawing.vue
+++ ui-vue/src/views/Drawing.vue
@@ -6,24 +6,37 @@
       v-on:mousedown="handleCanvasMouseDown"
       v-on:mouseup="handleCanvasMouseUp"
       v-on:mousemove="handleCanvasMouseMove"
-    />
+    >
+      <DrawingBox
+        v-for="boxObj in drawingBoxes"
+        :key="boxObj.id"
+        :top="boxObj.top"
+        :left="boxObj.left"
+        :width="boxObj.width"
+        :height="boxObj.height"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import Toolbar from "@/components/Toolbar.vue";
+import DrawingBox from "@/components/DrawingBox.vue";
+
 export default {
   name: "drawing",
   components: {
-    Toolbar
+    Toolbar,
+    DrawingBox
   },
   data() {
     return {
       currentTool: null, // current method on the toolbar
       drawingElement: false, // boolean defining whether or not an element is being drawn
-      currentBox: null, // HTML object of the current element being drawn
+      currentBoxProps: null, // Properties of the box currently being drawn
       currentElementContainer: null, // Container of the current element to calculate coordinates
-      occupiedY: 0 // Due to HTML nature, the elements get pushed further and further down when more appear. This variable tracks how much, to oppose it.
+      occupiedY: 0, // Due to HTML nature, the elements get pushed further and further down when more appear. This variable tracks how much, to oppose it.
+      drawingBoxes: [] // Array of drawn boxes
     };
   },
   methods: {
@@ -34,25 +47,26 @@ export default {
       if (this.currentTool) {
         if (this.currentTool === "box") {
           this.drawingElement = true;
-          // Creating drawn element and its container
+          // Creating drawn element and its container (to calculate coordinates)
           this.currentElementContainer = event.target.getBoundingClientRect();
-          const box = document.createElement("div");
-          box.className = "drawing-box";
+          const boxProps = {};
           // Calculating coordinates and passing them in as left/top values on a position=relative div
-          box.style.left = `${event.clientX -
-            this.currentElementContainer.x}px`;
-          box.style.top = `${event.clientY -
+          boxProps.left = `${event.clientX - this.currentElementContainer.x}px`;
+          boxProps.top = `${event.clientY -
             this.currentElementContainer.y -
             this.occupiedY}px`;
+          // Adding a default height and width in case mouse won't be moved
+          boxProps.width = "5px";
+          boxProps.height = "5px";
           // Saving created element to data()
-          this.currentBox = box;
+          this.currentBoxProps = boxProps;
         }
       }
     },
     handleCanvasMouseMove(event) {
       if (this.drawingElement) {
-        const boxX = Number(this.currentBox.style.left.replace("px", ""));
-        const boxY = Number(this.currentBox.style.top.replace("px", ""));
+        const boxX = Number(this.currentBoxProps.left.replace("px", ""));
+        const boxY = Number(this.currentBoxProps.top.replace("px", ""));
         // Calculating mouse position compared to the first mouse position as width and height
         const width =
           event.clientX - this.currentElementContainer.x - boxX + "px";
@@ -62,21 +76,22 @@ export default {
           boxY -
           this.occupiedY +
           "px";
-        this.currentBox.style.width = width;
-        this.currentBox.style.height = height;
+        this.currentBoxProps.width = width;
+        this.currentBoxProps.height = height;
       }
     },
     handleCanvasMouseUp(event) {
       if (this.drawingElement) {
-        event.target.appendChild(this.currentBox);
+        this.drawingBoxes.push({
+          ...this.currentBoxProps,
+          id: this.drawingBoxes.length
+        });
         // Updating the occupiedY value with the box height
-        const boxHeight = Number(
-          this.currentBox.style.height.replace("px", "")
-        );
+        const boxHeight = Number(this.currentBoxProps.height.replace("px", ""));
         this.occupiedY += boxHeight;
         // Reset
         this.drawingElement = false;
-        this.currentBox = null;
+        this.currentBoxProps = {};
       }
     }
   }
@@ -97,13 +112,5 @@ export default {
   width: 90%;
   height: 90%;
   background-color: rgb(70, 70, 235);
-}
-</style>
-
-<style>
-.drawing .canvas .drawing-box {
-  height: 5px;
-  background-color: red;
-  position: relative;
 }
 </style>
