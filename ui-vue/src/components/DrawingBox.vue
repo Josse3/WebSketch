ecfe6e4ddb325fda565763e2217741dcdb5870a7
Josse3 <josse.cottenier@hotmail.com>
2019-07-21 15:29:34 +0200

    Added component replacing div as drawing box

---
+++ ui-vue/src/components/DrawingBox.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="drawing-box" :style="{top, left, width, height}"></div>
+</template>
+
+<script>
+export default {
+  name: "DrawingBox",
+  props: {
+    top: String,
+    left: String,
+    width: String,
+    height: String
+  }
+};
+</script>
+
+
+<style scoped>
+.drawing-box {
+  height: 5px;
+  background-color: red;
+  position: relative;
+}
+</style>
