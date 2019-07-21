7770a3b29ff7de7f8a719f4c43cb16d7b0373dd1
Josse3 <josse.cottenier@hotmail.com>
2019-07-21 09:44:27 +0200

    Toolbar with basic sketching tools

---
+++ ui-vue/src/components/Toolbar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="toolbar">
+    <p v-for="tool in tools" v-bind:key="tool.id" v-on:click="selectTool">
+      <i :class="tool.icon" />
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Toolbar",
+  data() {
+    return {
+      tools: [
+        { icon: "far fa-square", id: "box" },
+        { icon: "glyphicon glyphicon-text-size", id: "text" }
+      ]
+    };
+  },
+  methods: {
+    selectTool: () => this.$parent.$emit("select-tool")
+  }
+};
+</script>
+
+<style scoped>
+.toolbar {
+  position: absolute;
+  top: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 4rem;
+  width: 80vw;
+  margin: auto;
+  background-color: rgba(80, 80, 80, 0.623);
+}
+
+.toolbar p {
+  font-size: 2.5rem;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  padding: 0.25em 1em;
+  margin: 0;
+}
+
+.toolbar p:hover {
+  background-color: rgb(153, 144, 144);
+}
+</style>
