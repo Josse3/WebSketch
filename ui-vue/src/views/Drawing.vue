e23f6b1b79a934bc29957786921cb276a12eb87d
Josse3 <josse.cottenier@hotmail.com>
2019-07-21 09:44:35 +0200

    Page with canvas for drawing

---
+++ ui-vue/src/views/Drawing.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="drawing">
+    <Toolbar />
+    <div class="canvas" v-on:select-tool="() => alert('select')"></div>
+  </div>
+</template>
+
+<script>
+import Toolbar from "@/components/Toolbar.vue";
+export default {
+  name: "drawing",
+  components: {
+    Toolbar
+  },
+  methods: {
+    useTool: () => alert("selected")
+  }
+};
+</script>
+
+<style scoped>
+.drawing {
+  width: 100vw;
+  height: 100vh;
+  background-color: lightblue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.canvas {
+  width: 90%;
+  height: 90%;
+  background-color: rgb(70, 70, 235);
+}
+</style>
