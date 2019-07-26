cf31eccd1a362bc4eeb22f49e3d4eaf253f67515
Josse3 <josse.cottenier@hotmail.com>
2019-07-26 19:52:26 +0200

    Added context menu interface for drawing boxes

--- ui-vue/src/components/ContextMenu-contents/Box.vue
+++ ui-vue/src/components/ContextMenu-contents/Box.vue
@@ -1,10 +1,64 @@
 <template>
-  <div class="context-menu-content-box"></div>
+  <div class="context-menu-content-box">
+    <div class="color-preview" :style="{backgroundColor: inputtedColor}"></div>
+    <input type="text" placeholder="color" @change="function(e) {inputtedColor = e.target.value}" />
+    <input type="submit" value="Apply" @click="handleColorSubmit" />
+    <span>Delete</span>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Box"
+  name: "Box",
+  props: {
+    id: Number
+  },
+  data() {
+    return {
+      inputtedColor: "white"
+    };
+  },
+  methods: {
+    handleColorSubmit() {
+      this.$emit("color-change", { color: this.inputtedColor, id: this.id });
+    }
+  }
 };
 </script>
 
+<style scoped>
+.context-menu-content-box {
+  width: 10rem;
+  text-align: center;
+}
+
+.color-preview {
+  width: 80%;
+  min-height: 7rem;
+  margin: auto;
+}
+
+input[type="text"] {
+  width: 80%;
+  margin-top: 0.75rem;
+}
+
+input[type="submit"] {
+  display: block;
+  margin: 0.75rem auto;
+  background-color: rgb(74, 74, 243);
+  padding: 0.25em;
+  border-radius: 0.25em;
+  border: none;
+}
+
+span,
+span:hover {
+  text-decoration: none;
+  color: white;
+}
+
+span {
+  margin-top: 50px;
+}
+</style>
