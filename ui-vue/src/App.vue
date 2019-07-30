4c8a16e41933a9fad5a7289f4b28ede2f7340052
Josse3 <josse.cottenier@hotmail.com>
2019-07-30 09:59:33 +0200

    Defined global styles and CSS variables

--- ui-vue/src/App.vue
+++ ui-vue/src/App.vue
@@ -5,6 +5,12 @@
 </template>
 
 <style>
+:root {
+  --ui-color-primary: rgb(74, 177, 74);
+  --ui-color-secondary: lightblue;
+  --button-color-blue: rgb(32, 32, 161);
+}
+
 * {
   box-sizing: border-box;
   padding: 0;
@@ -13,5 +19,15 @@
 
 body {
   font-family: sans-serif;
+}
+
+.error {
+  background-color: rgb(216, 104, 104);
+  border: 1px solid red;
+}
+
+.succes {
+  background-color: rgb(98, 197, 98);
+  border: 1px solid green;
 }
 </style>
