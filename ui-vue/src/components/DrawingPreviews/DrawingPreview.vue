ab120cf77ef05b1a37342df84fb2a9b6f8653840
Josse3 <josse.cottenier@hotmail.com>
2019-08-02 16:17:07 +0200

    Completed layer change + previewbox as separate component

---
+++ ui-vue/src/components/DrawingPreviews/DrawingPreview.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="preview-box" :style="{width, height, top, left}"></div>
+</template>
+
+<script>
+export default {
+  name: "DrawingPreview",
+  props: {
+    width: String,
+    height: String,
+    top: String,
+    left: String
+  }
+};
+</script>
+
+
+<style scoped>
+.preview-box {
+  position: relative;
+  border: 1px dashed red;
+}
+</style>
--- ui-vue/src/views/Drawing.vue
+++ ui-vue/src/views/Drawing.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="drawing">
-    <Toolbar 
-      v-on:select-tool="handleToolSelect" 
-      @change-canvas-color="handleCanvasColorChange" />
+    <Toolbar
+      v-on:select-tool="handleToolSelect"
+      :minLayer="currentHighestLayer"
+      @change-canvas-color="handleCanvasColorChange"
+    />
     <div
       class="canvas"
       v-on:mousedown="handleCanvasMouseDown"
@@ -23,6 +25,13 @@
         :id="boxObj.id"
         @show-contextmenu="handleContextMenuShow"
       />
+      <DrawingPreview
+        v-if="drawingElement"
+        :top="currentPreviewBoxProps.top"
+        :left="currentPreviewBoxProps.left"
+        :width="currentPreviewBoxProps.width"
+        :height="currentPreviewBoxProps.height"
+      />
       <ContextMenu
         v-if="contextMenuShown"
         :x="this.contextMenuProps.x"
@@ -41,6 +50,7 @@
 <script>
 import Toolbar from "@/components/Toolbar.vue";
 import DrawingBox from "@/components/DrawingBox.vue";
+import DrawingPreview from "@/components/DrawingPreview.vue";
 import ContextMenu from "@/components/ContextMenu.vue";
 
 export default {
@@ -48,18 +58,19 @@ export default {
   components: {
     Toolbar,
     DrawingBox,
+    DrawingPreview,
     ContextMenu
   },
   data() {
     return {
-      canvasColor: '#ccc', // Background color of the canvas
+      canvasColor: "#ccc", // Background color of the canvas
       currentTool: null, // current method on the toolbar
       drawingElement: false, // boolean defining whether or not an element is being drawn
       currentBoxProps: null, // Properties of the box currently being drawn
       currentElementContainer: null, // Container of the current element to calculate coordinates
       occupiedY: 0, // Due to HTML nature, the elements get pushed further and further down when more appear. This variable tracks how much, to oppose it.
       drawingBoxes: [], // Array of drawn boxes
-      currentPreviewBox: null, // HTML object of the current preview box
+      currentPreviewBoxProps: {}, // Object with properties of the current preview box
       contextMenuShown: false, // Boolean defining wheather or not the context menu is shown
       contextMenuProps: {}, // Object containing x and y coordinates as well as content of the context menu
       currentHighestLayer: 0 // Number defining the current highest layer (z-index);
@@ -71,30 +82,32 @@ export default {
     },
     handleCanvasMouseDown(event) {
       if (this.currentTool && event.which === 1 && !this.contextMenuShown) {
+        // Calculating coordinates
+        const left = `${event.clientX - this.currentElementContainer.x}px`;
+        const top = `${event.clientY -
+          this.currentElementContainer.y -
+          this.occupiedY}px`;
+
         if (this.currentTool === "box") {
           this.drawingElement = true;
           // Creating drawn element and its container (to calculate coordinates)
-          this.currentElementContainer = event.target.getBoundingClientRect();
+          this.currentElementContainer =
+            event.path.length > 7
+              ? event.srcElement.parentNode.getBoundingClientRect()
+              : event.target.getBoundingClientRect();
           const boxProps = {};
-          // Calculating coordinates
-          const left = `${event.clientX - this.currentElementContainer.x}px`;
-          const top = `${event.clientY -
-            this.currentElementContainer.y -
-            this.occupiedY}px`;
           // Passing them in as left/top values on a position="relative" <div>
           boxProps.left = left;
-          boxProps.top = Number(top.replace('px', '')) - this.occupiedY;
+          boxProps.top = Number(top.replace("px", "")) - this.occupiedY;
           boxProps.offset = this.occupiedY;
-          // Creating a preview box with the same coordinates
-          const previewBox = document.createElement("div");
-          previewBox.className = "preview-box";
-          previewBox.style.left = left;
-          previewBox.style.top = top;
-          this.currentPreviewBox = previewBox;
-          event.target.appendChild(this.currentPreviewBox);
+          // Setting the preview box coordinates
+          this.currentPreviewBoxProps.left = left;
+          this.currentPreviewBoxProps.top = top;
           // Adding a default height and width in case mouse won't be moved
           boxProps.width = "5px";
           boxProps.height = "5px";
+          this.currentPreviewBoxProps.width = "5px";
+          this.currentPreviewBoxProps.height = "5px";
           // Adding a default layer (z-index)
           boxProps.layer = this.currentHighestLayer;
           this.currentHighestLayer++;
@@ -122,15 +135,14 @@ export default {
         this.currentBoxProps.width = width;
         this.currentBoxProps.height = height;
         // Updating the (visible) preview box's property
-        this.currentPreviewBox.style.width = width;
-        this.currentPreviewBox.style.height = height;
+        this.currentPreviewBoxProps.width = width;
+        this.currentPreviewBoxProps.height = height;
       }
     },
     handleCanvasMouseUp(event) {
       if (this.drawingElement) {
         // Removing preview box
-        event.target.removeChild(this.currentPreviewBox);
-        this.currentPreviewBox = null;
+        this.currentPreviewBoxProps = {};
         // Setting default color
         this.currentBoxProps.color = "red";
         this.drawingBoxes.push({
@@ -159,27 +171,34 @@ export default {
       this.drawingBoxes[boxesArrayIndex] = changedBox;
     },
     handleDeleteBox(details) {
-      const offsetToRemove = Number(this.drawingBoxes[details].height.replace('px', ''));
+      const offsetToRemove = Number(
+        this.drawingBoxes[details].height.replace("px", "")
+      );
       this.drawingBoxes.splice(details, 1);
-      this.drawingBoxes.forEach(drawingBox => drawingBox.offset += offsetToRemove);
+      this.drawingBoxes.forEach(
+        drawingBox => (drawingBox.offset += offsetToRemove)
+      );
     },
     handleLayerChange(details) {
       const usedLayers = this.drawingBoxes.map(box => box.layer);
-      console.log(this.drawingBoxes, usedLayers);
-      const currentIndex = usedLayers.indexOf(this.drawingBoxes[details.id].layer);
+      const currentIndex = usedLayers.indexOf(
+        this.drawingBoxes[details.id].layer
+      );
       let newLayer;
-      if (details.direction === 'higher') {
+      if (details.direction === "higher") {
         const nextLayer = usedLayers[currentIndex + 1];
-        newLayer = !Number.isNaN(nextLayer) ? nextLayer + 1 : usedLayers[usedLayers.length - 1] + 1;
-      } else if (details.direction === 'lower') {
+        newLayer = nextLayer
+          ? nextLayer + 1
+          : usedLayers[usedLayers.length - 1] + 1;
+      } else if (details.direction === "lower") {
         const previousLayer = usedLayers[currentIndex - 1];
-        newLayer = !Number.isNaN(previousLayer) ? previousLayer - 1 : usedLayers[0] - 1;
-      } else if (details.direction === 'highest') {
+        newLayer = previousLayer ? previousLayer - 1 : usedLayers[0] - 1;
+      } else if (details.direction === "highest") {
         newLayer = usedLayers[usedLayer.length - 1] + 1;
-      } else if (details.direction === 'lowest') {
+      } else if (details.direction === "lowest") {
         newLayer = usedLayers[0] - 1;
       } else {
-        throw Error('Invalid layer change direction');
+        throw Error("Invalid layer change direction");
       }
       this.drawingBoxes[details.id].layer = newLayer;
     },
@@ -200,12 +219,5 @@ export default {
 .canvas {
   width: 100vw;
   height: 100vh;
-}
-</style>
-
-<style>
-.drawing .canvas .preview-box {
-  position: relative;
-  border: 1px dashed red;
 }
 </style>
